9e8c0da8cd2bfc6bdf794042cc62cdcfb05ef39d
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-16 07:27:09 -0400

    Set db's extension to '.db3'

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/recipes.sqlite3'
+      filename: './src/database/recipes.db3'
     },
     migrations: {
       directory: './src/database/migrations'
